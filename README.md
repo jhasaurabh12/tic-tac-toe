c8e41f0fa811d85fcddb902c1ff86d948b27b1be
Saurabh Jha <jhasaurabh12@gmail.com>
2024-03-04 16:35:39 +0530

    Initial setup is done. Included tailwindcss, webpack, htmlwebpack plugin.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tic-tac-toe
 Inflection point in learning Frontend
+
+# For Webpack and Tailwind CSS setup
+https://gist.github.com/bradtraversy/1c93938c1fe4f10d1e5b0532ae22e16a
+
+# To setup HTMLWebpackplugin
+This helps in generating html file in dist folder which has all the required dependencies linked in the html file
+https://webpack.js.org/guides/output-management/#setting-up-htmlwebpackplugin 
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+mode:"development",
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean:true,
+  },
+  devtool:"inline-source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template:'./src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+};
